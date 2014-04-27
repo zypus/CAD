56c75f8c9aaf02c4fad47d009ad72f909a245378
Nathan <Dobbien@users.noreply.github.com>
2014-04-27 19:45:41 +0200

    Update BezierSpline.java
    Method for calculating a point given a double u should be complete. Will discuss further with fabian regarding point interface in person to ensure operation

--- src/splines/BezierSpline.java
+++ src/splines/BezierSpline.java
@@ -1,11 +1,38 @@
 package splines;
 
+import java.util.ArrayList;
+
 /**
  * Created by fabian on 19/04/14.
+ * Edited by nathan on 26/04/14.
  */
 public class BezierSpline extends Spline {
+	
+	public BezierSpline(){}
+	
     @Override
     public Point s(double u) {
-        return null;
+    	double t = interval.getXSpan() / u;
+    	int k = size()-1;
+    	Point p;
+    	
+    	for (int i=0; i<k; i++){
+    		double scale = binom(k,i) * Math.pow(1-t, k-i);
+    		p = p.addValue(get(i).manipulate(scale));
+    	}
+    	p = p.addValue(get(k).manipulate(t^k));
+    	
+        return p;
+    }
+    
+    private double binom(int n, int k){
+    	double coeff = 1;
+    	for (int i= n-k+1; i<=n; i++){
+    		coeff *= i;
+    	}
+    	for (int i = 1; i<k; i++){
+    		coeff /= i;
+    	}
+    	return coeff;
     }
 }
