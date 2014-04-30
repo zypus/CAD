cc08b384ca18d9c7f778e0f6aa7fe58cfbc52b55
Nathan <Dobbien@users.noreply.github.com>
2014-04-30 16:02:26 +0200

    Update BezierSpline.java
    Updated operations

--- src/splines/BezierSpline.java
+++ src/splines/BezierSpline.java
@@ -1,5 +1,7 @@
 package splines;
 
+import java.util.ArrayList;
+
 /**
  * Created by fabian on 19/04/14.
  * Edited by nathan on 26/04/14.
@@ -10,25 +12,36 @@ public class BezierSpline extends Spline {
 
     @Override
     public Point s(double u) {
-    	double t = getInterval().getXSpan() / u;
-    	int k = size()-1;
-    	Point p = get(0).createPoint(0,0);
-
-    	for (int i=0; i<k; i++){
-    		double scale = binom(k,i) * Math.pow(1-t, k-i);
-    		p = p.addValue(get(i).manipulate(scale));
+    
+    	
+    	int intPart = (int) Math.floor(u);
+    	double percent = u - intPart;
+    	
+    	int leftIndex = intPart;
+    	while(leftIndex%3!=0){
+    		leftIndex-=1;
     	}
-    	p = p.addValue(get(k).manipulate(Math.pow(t,k)));
-
-        return p;
+    	
+    	Point leftKnot = get(leftIndex);
+    	Point rightKnot = get(leftIndex+3);
+    	double t = percent;
+    	
+    	Point p = leftKnot.createPoint(0,0);
+    	for (int i = 0; i < 3 ; i++){
+    		p = p.addValue(get(leftIndex+i).manipulate(binom(3,i)*Math.pow(t,i)*Math.pow(1-t,3-i)));
+    	}
+    	return p;
     }
 
     private double binom(int n, int k){
     	double coeff = 1;
-    	for (int i= n-k+1; i<=n; i++){
+    	for (int i = 1; i<=n; i++){
     		coeff *= i;
     	}
-    	for (int i = 1; i<k; i++){
+    	for (int i = 1; i<=k; i++){
+    		coeff /= i;
+    	}
+    	for (int i = 1; i<=(n-k); i++){
     		coeff /= i;
     	}
     	return coeff;
