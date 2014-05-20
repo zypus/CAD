8f0c5f18b4bb196c0ee9ebe1b9073532d3dc10b2
Nathan <Dobbien@users.noreply.github.com>
2014-05-20 20:28:56 +0200

    Working splinearea
    About to check the intervals and make it work for closed cubics

--- src/splines/SplineArea.java
+++ src/splines/SplineArea.java
@@ -1,12 +1,16 @@
 package splines;
 
+import java.awt.Rectangle;
 import java.util.ArrayList;
+
+import util.integration.ParametrizedTrapezoidRule;
+import gui.Spline2D;
 
 /**
  * Created by fabian on 19/04/14.
  */
 public class SplineArea implements SplineProperty {
-	private int STEPS = 1000;
+	private int STEPS = 10;
 	private Spline spline;
 	@Override
 	public double getValue(Spline spline) {
@@ -14,23 +18,47 @@ public class SplineArea implements SplineProperty {
 
 		ArrayList<Double> reflexU = getIntervals();
 		double length = 0;
-		
 		for(int i = 0;i<reflexU.size()-1;i++){
-			length += areaOfInterval(reflexU.get(i),reflexU.get(i+1)); //alternate
+			length += areaOfInterval(reflexU.get(i),reflexU.get(i+1));
+			System.out.println("this is an interval ");
+		}
+		return Math.abs(length);
+	}
+	
+	private double areaOfInterval(double uMin, double uMax){
+		Spline2D d2 = new Spline2D(spline, null);
+		Rectangle.Double rect = d2.getBoundingBox();
+		ParametrizedTrapezoidRule paraRule = new ParametrizedTrapezoidRule();
+		double h[] = new double[STEPS];
+		for (int i=0;i<STEPS;i++){
+			h[i] = (Math.abs(uMax-uMin)) / (Math.pow(2, i-1));
 		}
 		
-		return Math.abs(length);
+		double R[][] = new double[STEPS][STEPS];
+		SplineFunctionX fx = new SplineFunctionX(spline);
+		SplineFunctionY fy = new SplineFunctionY(spline);
+		for (int k = 0; k< STEPS;k++){
+			R[k][0] = paraRule.integrate(fy, fx, uMin, uMax, (int)Math.pow(2,k));
+		}
+		for (int m = 1;m<STEPS;m++){
+			for (int n = m;n<STEPS;n++){
+				R[n][m] = R[n][m-1] + (1/(Math.pow(4,m)-1)) * (R[n][m-1] - R[n-1][m-1]);
+			}			
+		}	
+		double area = rect.getMaxY()*rect.width;
+		return R[STEPS-1][STEPS-1] - area;
+		
 	}
 
 	private ArrayList<Double> getIntervals(){
-		SplineLength lengthCalc = new SplineLength();
-		double length = lengthCalc.getValue(spline);
+		double length = spline.size()-1;
 		double increment = length/STEPS;
 		ArrayList<Double> reflexPoints = new ArrayList();
 
 		double xGap = spline.s(0).getX() + spline.s(increment).getX();
 		Point p = spline.s(increment);
-
+		
+		reflexPoints.add(0.0);
 		for (double i = 2*increment;i< length; i += increment){
 			Point q = spline.s(i);
 			double newGap = q.getX() - p.getX();
@@ -41,6 +69,7 @@ public class SplineArea implements SplineProperty {
 			xGap = newGap;
 			p = q;			
 		}
+		reflexPoints.add((double)spline.size()-1);
 
 		return reflexPoints;
 	}
@@ -49,33 +78,6 @@ public class SplineArea implements SplineProperty {
 		return ((xGap<0) == (newGap<0));
 	}
 
-	private double areaOfInterval(double uMin, double uMax){
-
-		double h[] = new double[STEPS];
-		for (int i=0;i<STEPS;i++){
-			h[i] = (Math.abs(uMax-uMin)) / (Math.pow(2, i-1));
-		}
-
-		double R[][] = new double[STEPS][STEPS];
-		R[0][0] = (h[0]/2)*(spline.s(uMin).getY() + spline.s(uMax).getY());
-
-		for (int k = 1;k<STEPS;k++){
-			double sum = 0;
-			for (int i = 1;i<Math.pow(2, STEPS-1)+1;i++){
-				sum+=spline.s(uMin +(2*i-1)*h[k]).getY();
-			}
-
-			R[k][0] = 0.5 * (R[k-1][0] + h[k-1] * sum);		
-		}
-
-		for (int m = 1;m<STEPS;m++){
-			for (int n = m;n<STEPS;n++){
-				R[n][m] = (1/(Math.pow(4,m)-1))*(Math.pow(4, m)*R[n][m-1] - R[n-1][m-1]);
-			}			
-		}
-		return R[STEPS-1][STEPS-1];
-
-	}
 
 	@Override
 	public String getName() {
